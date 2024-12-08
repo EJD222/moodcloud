cdb66e6f4122833757d294bba8488fab1d2482fc
EJD222 <eljdiaz500@gmail.com>
2024-12-08 22:41:59 +0800

    Change to mysql and add user_id

--- database/migrations/2024_12_03_165453_create_sentiment_histories_table.php
+++ database/migrations/2024_12_03_165453_create_sentiment_histories_table.php
@@ -10,6 +10,8 @@ class CreateSentimentHistoriesTable extends Migration
     {
         Schema::create('sentiment_histories', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id'); // Use unsignedBigInteger for foreign keys
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade'); // Add foreign key constraint
             $table->string('sentiment');
             $table->integer('score');
             $table->string('file_name')->nullable();
