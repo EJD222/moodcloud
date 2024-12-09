39bf8c494f2c94e0912fa572b6c6413dbc8bda27
tinebasilio <kristinebasilio28@gmail.com>
2024-12-09 16:17:05 +0800

    Add how it works section

--- resources/views/partials/how-it-works.blade.php
+++ resources/views/partials/how-it-works.blade.php
@@ -4,19 +4,19 @@
             <h6 class="text-warning mb-3">How it works</h5>
 
                 <div class="feature-box px-3">
-                    <span class="card-icon bg-danger text-white rounded-circle">1</span>
+                    <span class="card-icon text-white rounded-circle" style="background-color: #8c52ff;">1</span>
                     <h3 class="mb-3">Type or Say Something</h3>
                     <p>Input your text or use our speech-to-text feature to start analyzing.</p>
                 </div>
 
                 <div class="feature-box px-3">
-                    <span class="card-icon bg-success text-white rounded-circle">2</span>
+                    <span class="card-icon text-white rounded-circle" style="background-color: #d21486;">2</span>
                     <h3 class="mb-3">Analyze Sentiment</h3>
                     <p>The tool processes the input to detect emotions and calculate sentiment scores.</p>
                 </div>
 
                 <div class="feature-box px-3">
-                    <span class="card-icon bg-primary text-white rounded-circle">3</span>
+                    <span class="card-icon text-white rounded-circle" style="background-color: #0097b2;">3</span>
                     <h3 class="mb-3"> View Analysis Results</h3>
                     <p>Get clear insights, including emotion labels and sentiment scores, instantly.</p>
                 </div>
