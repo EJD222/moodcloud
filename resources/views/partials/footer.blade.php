e6e58224ba973553926f83e3e2fa74a73377510a
tinebasilio <kristinebasilio28@gmail.com>
2024-12-09 19:40:05 +0800

    Change footer details

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -8,11 +8,7 @@
                     class="text-dark pl-1">Mood Cloud</a>
             </div>
             <div class="d-flex justify-content-start">
-                <span class="text-small text-dark mx-0 footer-link">Free </span>
-                <a href="https://www.bootstrapdash.com/ui-kit/" target="_blank"
-                    class="text-small text-dark mx-1 footer-link">UI kits </a>
-                <span href="#" class="text-small text-dark mx-0 footer-link">from</span> <a
-                    href="https://www.bootstrapdash.com/" target="_blank" class="text-dark pl-1"> Bootstrapdash.com</a>
+                <span class="text-small text-dark mx-0 footer-link">A Final Project by BSIT-4 Students, Deployed on Microsoft Azure</span>
             </div>
         </nav>
     </div>
